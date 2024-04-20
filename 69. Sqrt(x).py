edd73bd2b2f499f24360c5933c944cc411f71fa0
Мирослав Христов <miroslav.valsorimhh@gmail.com>
2024-04-20 10:39:06 +0300

    plus one

---
+++ 69. Sqrt(x).py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def mySqrt(self, x):
+        if x == 0:
+            return 0
+
+        left = 1
+        right = x
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return right
+
+
+solution = Solution()
+s = solution.mySqrt(8)
+print(s)
